1df9d133aa086e54878c6f1fd44aeecca9ecfd20
Azvadennys Vasiguhamiaz <azvadenis@gmail.com>
2024-01-26 11:23:54 +0700

    Update new requirements

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from google.cloud import storage
 from io import BytesIO
 import numpy as np
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 # Define class names
 class_names = ['Coelogyne', 'Cymbidium', 'Dendrobium', 'Phalaenopsis','Vanda']
